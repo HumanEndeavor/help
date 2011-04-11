b5d028cd13a8df9aa8bcd9fb5d2752b68748ebda
Puneet Pandey <puneet_pandey85@yahoo.co.in>
2011-04-11 21:08:58 +0530

    Human Endeavor to Love People - First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Help::Application.config.secret_token = '8b49873ed8dea2c7958aad6a046999f3c22c3ccaebf246a040ebdfe89be801b613cc830a539af9438165a72e1605c8bcae25120119d11472b5212446c385de3f'
