31c6a05987ad130f2e5b4fe1228f3bf0511fe538
unknown <Ankit Gupta-FIR@.ads.mckinsey.com>
2011-07-12 19:07:39 +0530

    Merge branch 'master' of https://github.com/HumanEndeavor/help
    Conflicts:
    	app/views/education/_form.html.erb
    
    	modified:   _form.html.erb
    	modified:   index.html.erb

--- app/controllers/education_controller.rb
+++ app/controllers/education_controller.rb
@@ -1,6 +1,7 @@
 class EducationController < ApplicationController
   def index
     @ngos = Ngo.where(:category_id => 1)
-    @states = State.all
+    @states = State.for_ngo(params[:ngo])
+    @districts = District.for_state(params[:state])
   end
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -2,4 +2,7 @@ class State < ActiveRecord::Base
   has_many :districts
   has_many :sponsors
   has_many :people
+  has_and_belongs_to_many :ngos
+
+  scope :for_ngo, lambda {|ngo| ngo ? where("ngo_id = ? ", ngo) : all }
 end
