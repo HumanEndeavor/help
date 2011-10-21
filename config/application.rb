eb0e01b7418d5dc2a563bf7c27f861302e491194
doel sengupta <doels11@gmail.com>
2011-10-21 20:10:41 +0530

    commented asset part

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module Help
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    #config.assets.enabled = false
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
